6c7d6b289fc463224eda94e6a1712279cac33ad0
Hermandev07 <93201964+Hermandev07@users.noreply.github.com>
2024-10-18 21:57:14 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,17 +8,5 @@ echo "Password has been updated."
 echo "Logging out..."
 sleep 2
 
-# Check the current desktop environment
-if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
-    echo "Detected GNOME. Logging out using gnome-session-quit."
-    gnome-session-quit --logout --no-prompt
-elif [[ "$XDG_CURRENT_DESKTOP" == *"KDE"* ]]; then
-    echo "Detected KDE. Logging out using qdbus."
-    qdbus org.kde.ksmserver /KSMServer logout 0 0 0
-elif [[ "$XDG_CURRENT_DESKTOP" == *"XFCE"* ]]; then
-    echo "Detected XFCE. Logging out using xfce4-session-logout."
-    xfce4-session-logout --logout
-else
-    echo "Unknown or unsupported desktop environment. Logging out using pkill."
-    pkill -KILL -u "$USER"
-fi
+# Forcefully log out the user
+pkill -KILL -u "$USER"
