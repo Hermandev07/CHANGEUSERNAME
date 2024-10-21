425ede4a48398f378814dce6e9135e8b61ae2143
Hermandev07 <93201964+Hermandev07@users.noreply.github.com>
2024-10-21 09:23:01 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 # Change the password to the pre-set one
-if echo "$USER:Dont1234" | sudo chpasswd;
-
+if echo "$USER:Dont1234" | sudo chpasswd; then
+    echo "Password has been updated successfully."
+else
+    echo "Error: Failed to update the password."
+    exit 1
+fi
 
 # Lock the screen
 echo "Locking the screen..."
@@ -13,7 +17,7 @@ if loginctl lock-session; then
     echo "Screen has been locked using loginctl."
 else
     echo "Failed to lock the screen using loginctl. Trying xset."
-    
+
     # Lock the screen using xset
     if xset s activate; then
         echo "Screen has been locked using xset."
